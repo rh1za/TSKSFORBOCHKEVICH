e87f0fb241af1fbb01329011360621224cd7fd68
shyshyshy <kesy1327@gmail.com>
2024-09-29 19:11:08 +0900

    Add files via upload

---
+++ tusk1.kt
@@ -0,0 +1,61 @@
+import kotlin.math.exp
+//1
+fun main() {
+    println("int - это целочисленный тип данных")
+    println("string - это строковый тип данных")
+//2
+    val a = 123.123
+    println(a)
+//3
+    val b = exp(1.0)
+    println(b)
+//4
+    val q = readLine()
+    println("Вы ввели число: $q")
+//5
+    val w = readLine()
+    println("$w - вот такое число вы ввели")
+//6
+    val z = 1
+    val x = 13
+    val c = 49
+    println("$z $x $c")
+//7
+    val p = 7
+    val l = 15
+    val m = 100
+    println("$p  $l  $m")
+//8
+    val first = readLine()
+    val second = readLine()
+    val third = readLine()
+    println("$first  $second  $third")
+//9
+    val one = readLine()
+    val two = readLine()
+    val three = readLine()
+    val four = readLine()
+    println("$one $two $three $four")
+//10
+    val qw = 50
+    val we = 10
+    println(qw)
+    println(we)
+//11
+    val qq = 5
+    val ww = 10
+    val ee = 21
+    println(qq)
+    println(ww)
+    println(ee)
+//12
+    val io = readLine()
+    val chen = readLine()
+    val bane = readLine()
+    val pudge = readLine()
+    println("$io")
+    println("$chen")
+    println("$bane")
+    println("$pudge")
+
+}
